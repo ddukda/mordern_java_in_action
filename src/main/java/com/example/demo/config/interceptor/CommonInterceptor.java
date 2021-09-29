0ff0fb4e0e8f989c14ba42f52addaabb8f4e4214
ddukda@gmail.com <ddukda@gmail.com>
2021-09-29 22:05:48 +0900

    add logback lib
    logback 은 dependency 를 추가 한 후 logback-spring.xml 세팅을 해야하고
    interceptor 는 WebMvcConfigurer 가 필요하다.

--- src/main/java/com/example/demo/config/interceptor/CommonInterceptor.java
+++ src/main/java/com/example/demo/config/interceptor/CommonInterceptor.java
@@ -1,7 +1,6 @@
 package com.example.demo.config.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,8 +18,7 @@ public class CommonInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.debug("CommonInterceptor : {}", request.getRequestURI());
-        System.out.println("CommonInterceptor : " + request.getRequestURI());
+        log.debug("log test CommonInterceptor : {}", request.getRequestURI());
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }
